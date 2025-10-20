8582cc2a6a4c7921337293a203709aa0703c0c39
Leehyeonwoo <hyeon.woo7789@gmail.com>
2025-10-20 19:46:58 +0900

    exception practice

---
+++ Test_2/src/ExceptionEx.java
@@ -0,0 +1,53 @@
+
+public class ExceptionEx {
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 0;
+		int r = 0;
+		
+		// 예외 처리 문법
+		try {
+			r = x/y;
+			System.out.println(r);
+		} catch (ArithmeticException e) { // (오류 종류)Exception 
+			// 예외를 잡았냈을 때 대응 코드
+			System.out.println("오류 발생");
+		}
+		
+		// 배열 예외 처리
+		int[] k = new int[3];
+		k[0] = 0;
+		k[1] = 10;
+		k[2] = 100;
+		
+		try {
+			System.out.println(k[3]);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("오류오류");
+			System.out.println(e);	// 예외 종류 간략하게 출력
+			// e.printStackTrace();
+		}
+		
+		// 다중 예외 처리
+		try {
+			int x1 = 10;
+			int x2 = 0;
+			int r2 = x1/x2;
+			
+			int[] xx1 = {10, 20, 30};
+			System.out.println(xx1[3]);
+			
+		} catch (Exception e) {
+			System.out.println("Excption으로 다중 예외 처리");
+			System.out.println(e);
+		}	
+//		catch (ArrayIndexOutOfBoundsException e) {
+//			
+//		} catch (ArithmeticException e) {
+//			
+//		}	
+		
+		System.out.println("end");
+	}
+}
+
