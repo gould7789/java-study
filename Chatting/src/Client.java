1f943256a52e98ed3e9a29e3562c14591317c9df
Leehyeonwoo <hyeon.woo7789@gmail.com>
2025-12-09 17:58:25 +0900

    multichatting practice

--- Chatting/src/Client.java
+++ Chatting/src/Client.java
@@ -1,49 +1,133 @@
-import java.awt.FlowLayout;
+//Client 파트 구현 목록
+//   1) 서버에 접속(Socket 생성) - 접속 버튼이 클릭되었을 때
+//   2) 서버로 메시지 송신
+//   3) 서버에서 보내는 메시지 수신
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
 
-// 채팅 메시지 입력창, 대화창이 기본적으로 제공되는 클라이언트 화면 구현
+
+
 class ClientUI extends JFrame {
-	JButton con; // 서버 접속을 위한 버튼
-	JTextField jtf; // 채팅 메시지 입력창
-	JTextArea jta; // 지난 채팅 메시지를 볼 수 있는 대화창
-	
-	public ClientUI() {
-		// 상위 클래스(여기서는 JFrame) 호출
-		super("클라이언트");
-		con = new JButton("접속");
-		jtf = new JTextField(20);
-		jta = new JTextArea(30, 30);
-		
-		// 화면 배치 관리자
-		FlowLayout layout = new FlowLayout();
-		setLayout(layout);	// 현재 화면에 배치 관리자 설정
-		add(jtf);	// 화면에 한줄 입력창 먼저 배치
-		add(con);	// 접속 버튼
-		add(jta);	// 대화창 배치
-		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	// 강제 종료 기능 활성화
-		setSize(400, 700);		// 화면 사이즈 조절
-		setLocation(250, 250);	// 화면 위치 조절
-		setVisible(true);
-		
-	}
+    JButton con; //서버 접속을 위한 버튼
+    JTextField jtf; //채팅 메시지 입력창
+    JTextArea jta;
+
+    Socket client; //서버와의 통신을 위한 종이컵
+    OutputStream os; //서버에 데이터 전송 위한 실
+    InputStream is;
+    //서버에 접속을 성공하고 나면 스레드를 구동시켜줘야 함
+    class ClientThread extends Thread {
+        public void run() {
+            //   3) 서버에서 보내는 메시지 수신
+            while (true) {
+                try {
+                    byte[] b = new byte[1024];
+                    is.read(b);
+                    //수신된 메시지 화면에 보여주기
+                    String str = new String(b);
+                    jta.append(str.trim() + '\n');
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+
+        }
+    }
+
+    public ClientUI() {
+        super("이것은 클라이언트");
+        con = new JButton("접속");
+        jtf = new JTextField(20);
+        jta = new JTextArea(30, 30);
+
+
+        //입력창 비활성화 코드
+        jtf.setEnabled(false);
+        
+        class MyListener implements ActionListener {
+            public void actionPerformed(ActionEvent e) {
+                //calllback method
+                //버튼이 클릭되거나 enter 키가 입력되면 호출되는
+                //콜백 메소드
+
+                //버튼이 눌려졌는지 enter키가 눌려졌는지 구분
+                //눌러진 버튼에 이름 가져오기
+                String str = e.getActionCommand();
+                if (str.equals("접속")) {
+
+                    try {//1) 서버에 접속(Socket 생성)
+                        client = new Socket("127.0.0.1", 8888);
+//                        client = new Socket("210.101.236.171", 8888);
+                        //서버에 접속 성공 시
+                        is = client.getInputStream();
+                        os = client.getOutputStream();
+                        new ClientThread().start();
+                        
+                        //입력창 비활성화 코드
+                        jtf.setEnabled(true);
+                        //버튼 비활성화 코드
+                        con.setEnabled(false);
+                        
+                        con.setText("접속 성공");
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+
+                    System.out.println("서버 접속 버튼 클릭됨");
+                } else {    //입력창에 Enter key 입력되었을때
+                    //2)서버로 메시지 송신
+                    try {
+                        OutputStream os = client.getOutputStream();
+                        String msg = jta.getText(); //입력창에서 문자열 가져오기
+                        os.write(msg.getBytes());
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+
+                }
+
+            }
+        }
+
+        FlowLayout layout = new FlowLayout(); //화면 배치 관리자
+        setLayout(layout); //현재 판때기에 배치 관리자 설정
+        add(con); //접속버튼
+        add(jtf); //판때기에 한 줄 입력창 먼저 배치
+        add(jta); //대화창 배치
+
+        MyListener m = new MyListener(); //감시자 객체화
+        con.addActionListener(m);
+
+        jtf.addActionListener(m);
+
+
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 700);
+        setLocation(250,250);
+        setVisible(true);
+
+    }
 }
 
 public class Client {
-	public static void main(String[] args) throws IOException, Exception {
-		new ClientUI();
-//		// 1. 서버에 접속(Socket 생성)
-//		Socket client = new Socket("127.0.0.1", 8888);
-//		// 2. 서버로 메시지 송신
-//		OutputStream os = client.getOutputStream();
-//		os.write("나는 내가 빛나는 별인 줄 알았어요");
-	}
+    public static void main(String[] args) throws IOException {
+        new ClientUI();
+        //   1) 서버에 접속(Socket 생성)
+//        Socket client = new Socket("127.0.0.1", 8888);
+//        OutputStream os = client.getOutputStream();
+    }
 }
+
+
--- Chatting/src/Server.java
+++ Chatting/src/Server.java
@@ -1,54 +1,70 @@
+// To Do List
+//1: 클라이언트 접속 대기
+//   -> 좁속 후 클라이언트와 매칭되는 소켓 생성
+//2: 접속한 모든 클라이언트로부터 메시지 수신
+//   -> 수신된 메시지를 모든 클라이언트로 송신
+//   : 클라이언트 수 + 1개의 Thread 필요
 
-// 서버 파트 구현 목록
-// 1) 클라이언트 접속 대기
-// 		- 접속 후 클라이언트와 매칭되는 종이컵(Socket) 생성
-// 2) 접속한 모든 클라이언트로부터 메시지 수신
-//		- 수신된 메시지를 모든 클라이언트로 송신
-// 클라이언트 수 + 1개의 Thread 필요
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
-// 서버가 클라이언트 접속대기도 하면서 클라이언트들이 보내는 메시지 수신도 하기 위해
-// 두 가지 작업을 동시에 처리해 줄 수 있도록 thread를 만듦
+//서버가 클라이언트 접속대기도 하면서 클라이언트들이 보내는 메시지 수신
+//두 가지 작업을 동시에 처리할 수 있도록 Thread 만들기
 class ServerThread extends Thread {
-	Socket server;
-	public ServerThread(Socket server) {
-		// 현재 클래스의 멤버변수 server, 현재 생성자로 받아온 server
-		this.server = server;
-	}
-	
-	public void run() {
-		// 2) 접속한 클라이언트로부터 메시지 수신
-		try {
-			// 종이컵에서 읽기 위한 실 뽑아내기
-			InputStream is = server.getInputStream();
-			byte[] b = new byte[1024];
-			// 1024 바이트를 읽어서 배열 b에 저장
-			is.read(b);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+
+    Socket server;
+    public ServerThread(Socket server) {
+        this.server = server;
+    }
+    
+    public void run() {
+        //2) 접속한 클라이언트로부터 지속적으로 메시지 수신
+        while (true) {
+            try {
+                //종이컵에서 읽기 위한 실 뽑기
+                InputStream is = server.getInputStream();
+                byte[] b = new byte[1024];
+                is.read(b); // 1024 byte 읽어서 배열 b에 저장
+                
+                //수신된 메시지를 모든 클라이언트로 송신
+                //종이컵 저장공간에서 종이컵을 하나씩 가져와서 해당 클라이언트로 메시지 전송
+                //즉, 어딘가에 여태 만든 종이컵이 저장되어 있는곳이 있어야 함
+
+                // 종이컵 저장 공간 (totalSocket)에서 종이컵 하나씩 가져와서
+                // 쓰기 위한 실(OutputStream)을 뽑아내서 데이터 전송
+                for (int i = 0 ; i<Server.totalSocket.size(); i++) {
+                    Socket temp = (Socket) Server.totalSocket.get(i);
+                    OutputStream os = temp.getOutputStream();
+                    os.write(b); //방금전에 읽은 1024 바이트 만큼의 데이터를 전송
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
-
-
 public class Server {
-	public static void main(String[] args) throws IOException {
-		
-		// 1) 클라이언트 접속을 지속적으로 대기
-		ServerSocket ss = new ServerSocket(8888);
-		while (true) {	// 여러 클라이언트 접속을 받아주기 위해 반복문 사용 
-			// 클라이언트 접속대기 + 접속하면 종이컵 만들어두기
-			Socket server = ss.accept();
-			
-			// 2) 접속한 모든 클라이언트로부터 메시지 수신
-			
-			
-		}
-		
-		
-	}
+    static ArrayList totalSocket = new ArrayList();
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Server starting...");
+        //1: 클라이언트 접속을 지속적으로 대기
+        ServerSocket ss = new ServerSocket(8888);
+        while (true) {
+            //클라이언트 접속 대기 + 접속하면 소켓 생성
+            Socket server = ss.accept();
+            //종이컵 저장
+            totalSocket.add(server);
+//          이제 클라이언트가 접속했으니
+//          해당 클라이언트 담당하는 Thread 만들어서 구동시켜 줘야 함
+            new ServerThread(server).start();
+        }
+    }
 }
